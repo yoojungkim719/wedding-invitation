5e3d81217c8fe5a97198b0d3282e14ba2d746836
yoojungkim719 <ichbinyoojung@gmail.com>
2026-09-13 16:33:37 +0900

    사진 추가

--- src/images/index.ts
+++ src/images/index.ts
@@ -11,6 +11,8 @@ import image9 from "./image9.png"
 import image10 from "./image10.png"
 import image11 from "./image11.png"
 import image12 from "./image12.png"
+import image13 from "./image13.png"
+import image14 from "./image14.png"
 
 /**
  * 메인 커버 이미지
@@ -33,4 +35,6 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+  image13,
+  image14
 ]
